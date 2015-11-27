ff92335f74155f8a6e17168d325b6c5114d299e6
tanteng <tanteng@gmail.com>
2015-11-28 00:42:57 +0800

    提交修改

--- dayegov/dayegov/spiders/HotSpider.py
+++ dayegov/dayegov/spiders/HotSpider.py
@@ -1,20 +1,29 @@
-from scrapy.spiders import Spider
+from scrapy.spiders import Spider,CrawlSpider, Rule
 from scrapy.selector import Selector
-
+from scrapy.linkextractors import LinkExtractor
 from dayegov.items import DayegovItem
 
 
-class HotSpider(Spider):
+class HotSpider(CrawlSpider):
     name = "hotfocus"
     allowed_domains = ["hbdaye.gov.cn"]
     start_urls = [
         "http://www.hbdaye.gov.cn/xwzx/bmdt/index.shtml",
     ]
+    # http://www.hbdaye.gov.cn/xwzx/bmdt/index_1.shtml
+    rules = [
+        Rule(LinkExtractor(allow=('index_\d+.shtml')),
+             callback="parse"),
+    ]
+
+    for page in range(1,50):
+        start_urls.append('http://www.hbdaye.gov.cn/xwzx/bmdt/index_' + str(page) + '.shtml')
+
+
 
     def parse(self, response):
         sel = Selector(response)
         articles = sel.xpath('//div[@class="list_content"]/div[@class="list_cloumn"]')
-        items = []
 
         for article in articles:
             item = DayegovItem()
@@ -22,6 +31,4 @@ class HotSpider(Spider):
             item['link'] = article.xpath('ul/li[1]/h2/a/@href').extract()
             item['description'] = article.xpath('ul/li[2]/p/text()').extract()
 
-            items.append(item)
-
-        return items
+            yield item
